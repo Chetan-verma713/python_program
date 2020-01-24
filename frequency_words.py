8eae9f0521c83c357e241c1723b0756634705879
Chetan-verma713 <60186285+Chetan-verma713@users.noreply.github.com>
2020-01-24 11:52:22 +0530

    Update frequency_words.py

--- frequency_words.py
+++ frequency_words.py
@@ -1,6 +1,8 @@
-a = input("Enter a string  ")
+a = input("Enter a string  ").split()
+k = []
 for i in a:
 	b = a.count(i)
-	if i==i+1:
-		continue
-	print(i,b)
+	if i not in k:
+		print(i,":",b)
+		k+=i
+	
