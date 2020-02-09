84b608122ecbfc51b6526a8c177e374aaa99cfd2
Chetan-verma713 <60186285+Chetan-verma713@users.noreply.github.com>
2020-02-09 18:16:51 +0530

    Add files via upload

---
+++ greater_words.py
@@ -0,0 +1,6 @@
+n1 = input("Enter a name : ")
+n2 = input("Enter another name : ")
+if len(n1) > len(n2):
+    print("Word_1 is greater than Word_2")
+else :
+    print("Word_2 is greater than Word_1")
