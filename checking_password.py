a22115fff0942885c19e3de70c06e33d8c7fa0c9
Chetan-verma713 <60186285+Chetan-verma713@users.noreply.github.com>
2020-02-11 21:44:58 +0530

    Add files via upload
    it`s an awesome program to check password ....

---
+++ checking_password.py
@@ -0,0 +1,8 @@
+password = "12345678"
+print("Hint => 1 to 8")
+n = input("Enter password => ")
+if n==password:
+    print("Welcome to python")
+else:
+    print("""Enter a valid password...
+Try again""")
